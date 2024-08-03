84cf462ba0c62b644d30956630426feb7be3f28d
MoidulHasan <antu.khan.988@gmail.com>
2024-08-04 01:57:37 +0600

    feat: login page created

---
+++ src/pages/admin/login.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient();
+const email = ref("");
+
+const signInWithOtp = async () => {
+  const { error } = await supabase.auth.signInWithOtp({
+    email: email.value,
+    options: {
+      emailRedirectTo: "http://localhost:3000/admin",
+    },
+  });
+  if (error) console.log(error);
+};
+</script>
+<template>
+  <div>
+    <button class="p-3" @click="signInWithOtp">Sign In with E-Mail</button>
+    <input v-model="email" type="email" />
+  </div>
+</template>
